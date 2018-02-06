5e621134fcb05790f96de2c750ef6968c9365074
juansvasquez <juansebvas@gmail.com>
2018-02-05 21:46:02 -0500

    Regex update, token constructor

--- Project One/lexer.js
+++ Project One/lexer.js
@@ -1,10 +1,22 @@
 function clean(s){
-	cleaner = s.replace(/\s/g, ""); //removes whitespace
-	cleanest = cleaner.replace(/\/\*.*\*\//g, ""); //removes comments
+	//removes whitespace (except newline) not in StringExpr
+	var cleaner = s.replace(/[\r\t\f\v ]+(?=([^"]*"[^"]*")*[^"]*$)/g, "");
+	var cleanest = cleaner.replace(/\/\*.*\*\//g, ""); //removes comments
 	return cleanest;
 }
 
+//Token constructor
+function Token(name, value){
+	this.name = name;
+	this.value = value;
+}
+
+
+// TODO: This function should create an ordered list of tokens 
+// and return a String version of it
 function lexer(s){
 	str = clean(s);
-	return str;
+	//var lastPosition
+	return str; //should return a printed list per token
+	//also errors and warnings
 }
